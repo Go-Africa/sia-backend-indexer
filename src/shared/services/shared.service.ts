f7eeab56a3ebdcfd13570cf72e0b245024babfcb
Dalois-30 <claudetapi@yahoo.com>
2023-11-28 14:22:10 +0100

    shared, optimize loop

--- src/shared/services/shared.service.ts
+++ src/shared/services/shared.service.ts
@@ -40,7 +40,7 @@ export class SharedService {
             do {
                 try {
                     this.logger.verbose("Checking consensus data");
-                    await new Promise(resolve => setTimeout(resolve, 1000));
+                    await new Promise(resolve => setTimeout(resolve, 3000));
                     this.response = await lastValueFrom(
                         this.httpService.get(url, {
                             headers,
